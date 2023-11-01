85ee3ee14dc423f64fad4f9227b97661f2606f9f
Akeanant Poomdeesittinon <66866324+tony007x@users.noreply.github.com>
2023-11-01 19:54:02 +0700

    Add files via upload

--- MatricTriangle.java
+++ MatricTriangle.java
@@ -5,34 +5,51 @@ public class MatricTriangle {
 
     public static void main(String[] args) {
 
-        int [][]box = {
-            {1,0,0,0},
-            {0,2,0,0},
-            {0,0,3,0},
-            {0,0,0,4},
+        int[][] box = {
+                { 1, 0, 0, 0 },
+                { 2, 3, 0, 0 },
+                { 4, 5, 6, 0 },
+                { 7, 8, 9, 9 },
         };
-        
-        System.out.println(triangleChecker(box));
+
+        int[][] box2 = {
+                { 1, 2, 3, 4 },
+                { 0, 5, 6, 7 },
+                { 0, 0, 8, 9 },
+                { 0, 0, 0, 9 },
+        };
+
+        System.out.println(triangleChecker(box2));
     }
 
-    public static boolean triangleChecker(int[][] arr){
+    public static boolean triangleChecker(int[][] arr) {
 
-        boolean ans = true;
+        int lineChecker = 0;
+        // CASE 1
         for (int i = 0; i < arr.length; i++) {
+
             int cnt = 0;
             for (int j = 0; j < arr[0].length; j++) {
-                if (arr[i][j]==0) {
-                    cnt++;
+                if (arr[i][i] != 0) {
+                    System.out.print(arr[i][j] + " ");
+                    if (arr[i][j] != 0) {
+                        cnt++;
+                    }
+                } else {
+                    return false;
                 }
             }
-            if(cnt == arr[0].length-1){
-                ans =true;
+            System.out.println();
+            if(cnt == arr[0].length-i || cnt == i+1){
+                lineChecker++;
+                System.out.println("Line: "+lineChecker);
             }
-            else{
-                ans=false;
-                break;
-            }
+
         }
-        return ans;
+        if (lineChecker == arr[0].length) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
